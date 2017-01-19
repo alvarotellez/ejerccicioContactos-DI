6990003dc8e2d3326b0e1f4f792ad231741b83f4
Álvaro Téllez Hidalgo <alvaro.tellez.hidalgo@gmail.com>
2017-01-19 20:12:49 +0100

    Ya solo falta el maestro detalle y estilos

--- ejercicio-copiaContacto/MainPage.xaml.cs
+++ ejercicio-copiaContacto/MainPage.xaml.cs
@@ -15,23 +15,28 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
-// La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace ejercicio_copiaContacto
 {
     /// <summary>
-    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage1 : Page
     {
         SystemNavigationManager systemNavigationManager;
-        public MainPage()
+        public MainPage1()
         {
             this.InitializeComponent();
+
+            MyFrame.Navigate(typeof(Contactos));
+            //TitleTextBlock.Text = "Financial";
+
         }
+
         private void HamburgerButton_Click(object sender, RoutedEventArgs e)
         {
-            splitPrincipal.IsPaneOpen = !splitPrincipal.IsPaneOpen;
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
         }
 
         private void appointmentsDetail_BackRequested(object sender, BackRequestedEventArgs e)
